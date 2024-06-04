ccad7c334e746cc4dcf0567cb5c89d6048682a88
Gustavo Dalanholli Salomão <gustavo.dalanholli.salomao@gmail.com>
2024-06-04 19:22:31 -0300

    Adicionado modulos de Animais, Pessoas e Doações ao app.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { dataSourceOptions } from './database/connection';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AnimalModule } from './animal/animal.module';
+import { DoacaoModule } from './doacao/doacao.module';
+import { PessoaModule } from './pessoa/pessoa.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(dataSourceOptions),
+    AnimalModule,
+    DoacaoModule,
+    PessoaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
